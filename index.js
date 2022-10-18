cedca98c3ea7aae134b0f18a250df92d6dda267c
Rachael Carpenter <carprl@outlook.com>
2022-10-18 15:04:50 -0500

    created varible for storing pdf names

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import "./assets/pspdfkit.js";
-
 // We need to inform PSPDFKit where to look for its library assets, i.e. the location of the `pspdfkit-lib` directory.
 const baseUrl = `${window.location.protocol}//${window.location.host}/assets/`;
+
+var PDFname = "design.pdf";
+
+// so onClick will change the pdfName variable. yay
 
 PSPDFKit.load({
 	baseUrl,
 	container: "#pspdfkit",
-	document: "pspdfkit-web-demo.pdf"
+	document: PDFname
 })
 .then(instance => {
 	console.log("PSPDFKit loaded", instance);
