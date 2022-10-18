9c119c36e9357e940804e598ad94582a8cb19503
Rachael Carpenter <carprl@outlook.com>
2022-10-18 18:31:58 -0500

    bug needs fix: importing function from js to html files

--- index.js
+++ index.js
@@ -1,10 +1,62 @@
 import "./assets/pspdfkit.js";
+
+  
+var PDFname = "design.pdf";
+var PDFarrayCounter = 0;
+const PDFs = ["design.pdf", "requirements.pdf", "projects.pdf"];
+
+
+// so onClick will change the pdfName variable. 
+export function increasePDF(){
+	console.log("reached desired function");
+	if (PDFarrayCounter < 2){
+		PDFarrayCounter = PDFarrayCounter + 1;
+		PDFname = PDFs[PDFarrayCounter];
+
+		PSPDFKit.unload(instance)
+
+	PSPDFKit.load({
+		baseUrl,
+		container: "#pspdfkit",
+		document: PDFname
+	})
+	.then(instance => {
+		console.log("PSPDFKit loaded", instance);
+	})
+	.catch(error => {
+		console.error(error.message);
+	});
+	}
+
+
+}
+
+export function decreasePDF(){
+	if (PDFarrayCounter > 0){
+		PDFarrayCounter = PDFarrayCounter -1;
+		PDFname = PDFs[PDFarrayCounter];
+
+		PSPDFKit.unload(instance)
+
+	PSPDFKit.load({
+		baseUrl,
+		container: "#pspdfkit",
+		document: PDFname
+	})
+	.then(instance => {
+		console.log("PSPDFKit loaded", instance);
+	})
+	.catch(error => {
+		console.error(error.message);
+	});
+	}
+
+}
+
+
 // We need to inform PSPDFKit where to look for its library assets, i.e. the location of the `pspdfkit-lib` directory.
 const baseUrl = `${window.location.protocol}//${window.location.host}/assets/`;
 
-var PDFname = "design.pdf";
-
-// so onClick will change the pdfName variable. yay
 
 PSPDFKit.load({
 	baseUrl,
