da320eaa728ef02ecd0363b4bed96411e07bed86
Cone-Virus <68671114+Cone-Virus@users.noreply.github.com>
2022-12-01 11:35:29 -0600

    Update algo.py
    Up Algo

--- ruby_app/algo/algo.py
+++ ruby_app/algo/algo.py
@@ -2,20 +2,18 @@ from modules.helper import get_pdfs, temp_dir, pre_process, post_process
 from modules.BB import Bounding_Box_Algorithm as process
 import os
 import sys
+PATH1 = sys.argv[1]
 
-PATH = sys.argv[1]
-
-PATH = "storage/" + PATH[0] + PATH[1] + "/" + PATH[2] + PATH[3]
+PATH = "storage/" + PATH1[0] + PATH1[1] + "/" + PATH1[2] + PATH1[3]
 temp_dir(PATH,0)
 print("STARTED")
-for pdf in get_pdfs(PATH):
-    os.rename(PATH + "/" + pdf,PATH + "/" + pdf + '.pdf')
-    pre_process(PATH, (PATH + "/" + pdf + '.pdf')) # Convert each page of PDF to images
-    print("PRE-DONE")
-    process(PATH) # Write bounding box on each image
-    print("PROCESS DONE")
-    post_process(PATH,pdf + '.pdf') # Convert images back into pdf
-    print("POST-DONE")
-    temp_dir(PATH,2)
-    os.rename(PATH + "/" + pdf + ".pdf",PATH + "/" + pdf)
+os.rename(PATH + "/" + PATH1,PATH + "/" + PATH1 + '.pdf')
+pre_process(PATH, (PATH + "/" + PATH1 + '.pdf')) # Convert each page of PDF to images
+print("PRE-DONE")
+process(PATH) # Write bounding box on each image
+print("PROCESS DONE")
+post_process(PATH,PATH1 + '.pdf') # Convert images back into pdf
+print("POST-DONE")
+temp_dir(PATH,2)
+os.rename(PATH + "/" + PATH1 + ".pdf",PATH + "/" + PATH1)
 print("ENDED")
